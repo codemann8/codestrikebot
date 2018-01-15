a5685631a846b50bc159b416f504aa3c06fe34d0
codemann8 <codemann8@gmail.com>
2018-01-14 20:24:57 -0800

    Completed Debug Tool's ability to grab bitmap from the main UI over WCF

---
+++ CodeStrikeBotUI/Services/CodeBotService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.ServiceModel;
+
+namespace CodeStrikeBot.Services
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall, ConfigurationName = "bitmapOverTcp")]
+    public class CodeBotService : ICodeBotService
+    {
+        #region Service Members
+        public Bitmap GetScreen(int screenId)
+        {
+            if (screenId >= 0 && screenId < Controller.Instance.sc.Length)
+            {
+                Screen s = Controller.Instance.sc[screenId];
+                if (s != null && s.SuperBitmap != null && s.SuperBitmap.Bitmap != null)
+                {
+                    return new Bitmap(s.SuperBitmap.Bitmap);
+                }
+            }
+            
+            return new Bitmap(0, 0);
+        }
+        #endregion
+    }
+}
